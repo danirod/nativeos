ed9cb0ec65b486c35c96bf61d240628c7d40afe6
Dani Rodríguez <dani@danirod.es>
2017-04-14 18:21:53 +0200

    Remove legacy code from main.c
    Legacy code is code that was used back in the day for whatever reason,
    usually just debugging to VGA to assert things were properly working,
    but that was never moved or deleted after it wasn't required.
    
    Most important change applied on this commit is that count_memory
    function has moved to paging.c. count_memory is a static function that
    parses the multiboot structure in order to count how many megabytes of
    memory has the computer. Because this function is only used for setting
    up the paging system, it has moved there.
    
    I have also removed a few headers that were included in main.c but not
    used and removed some dead code from kmain: screen is now kept black,
    and instead of starting a dummy echo server on the serial port, an
    infinite loop is entered.

--- kernel/main.c
+++ kernel/main.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of NativeOS: next-gen x86 operating system
- * Copyright (C) 2015-2016 Dani Rodríguez
+ * Copyright (C) 2015-2017 Dani Rodríguez
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,36 +23,14 @@
 #include <arch/x86/gdt.h>
 #include <arch/x86/idt.h>
 #include <arch/x86/paging.h>
-#include <kernel/kernel.h>
-#include <kernel/memory.h>
-#include <kernel/multiboot.h>
 #include <driver/com.h>
 #include <driver/keyboard.h>
 #include <driver/timer.h>
 #include <driver/vbe.h>
-#include <driver/vga.h>
-
-extern int kernel_start;
-extern int kernel_after;
+#include <kernel/kernel.h>
+#include <kernel/multiboot.h>
 
 #define LOG(msg) (serial_send_str(COM_PORT_1, msg));
-
-/** 
- * @brief Calculate the amount of memory that this computer has.
- * @param multiboot_info Multiboot structure (it knows everything).
- * @return the amount of KB this PC has or -1 if we cannot know.
- */
-static int count_memory(multiboot_info_t *multiboot_info)
-{
-    if (multiboot_info->flags & 0x01) {
-        unsigned int start = multiboot_info->mem_lower;
-        /* Multiboot reports the ending area minus 1 MB. Increment it. */
-        unsigned int end = multiboot_info->mem_upper + 1024;
-        return end - start;
-    } else {
-        return -1; /* We cannot know. */
-    }
-}
 
 /**
  * @brief Main routine for the NativeOS Kernel.
@@ -95,50 +73,15 @@ void kmain(unsigned int magic_number, multiboot_info_t *multiboot_ptr)
 		for(;;);
 	}
 
-	// Init VBE framebuffer.
 	vbe_init(multiboot_ptr);
+	frames_init(multiboot_ptr);
 
 	int i;
 	for (i = 0; i < 16; i++)
 		idt_set_handler(i, &bsod);
 
-	for (int y = 0; y < 480; y++) {
-		for (int x = 0; x < 80; x++) {
-			vbe_putpixel(x, y, 255, 255, 255);
-		}
-		for (int x = 80; x < 160; x++) {
-			vbe_putpixel(x, y, 255, 255, 0);
-		}
-		for (int x = 160; x < 240; x++) {
-			vbe_putpixel(x, y, 0, 255, 255);
-		}
-		for (int x = 240; x < 320; x++) {
-			vbe_putpixel(x, y, 0, 255, 0);
-		}
-		for (int x = 320; x < 400; x++) {
-			vbe_putpixel(x, y, 255, 0, 255);
-		}
-		for (int x = 400; x < 480; x++) {
-			vbe_putpixel(x, y, 255, 0, 0);
-		}
-		for (int x = 480; x < 560; x++) {
-			vbe_putpixel(x, y, 0, 0, 255);
-		}
-		for (int x = 560; x < 640; x++) {
-			vbe_putpixel(x, y, 0, 0, 0);
-		}
-	}
-
-    unsigned int memory_amount = count_memory(multiboot_ptr);
-    frames_init(memory_amount);
-
-	LOG("NativeOS is starting...\n");
-
-	/* Dumb echo system for bytes read through COM1. */
-	for(;;) {
-		while (serial_get_recv_status(COM_PORT_1)) {
-			unsigned char byte = serial_recv_byte(COM_PORT_1);
-			serial_send_byte(COM_PORT_1, byte);
-		}
-	}
+	LOG("NativeOS has started.\n");
+	LOG("However, there is nothing to do.\n");
+	
+	for (;;);
 }
