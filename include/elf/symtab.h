1ec48aa98cdae94d4b394b5fe175b8b0213e96d4
Dani Rodríguez <dani@danirod.es>
2018-07-29 12:53:04 +0200

    Implement a basic symbol table
    The symbol table is used to register public symbols present in ELF
    object files as well as the memory addresses for these symbols.  Once
    this feature gets finalized, the symbol table will be loaded with the
    public functions available in nativeos.elf as well as other kernel
    extensions.
    
    Kernel extensions are additional and relocatable ELF objects.  When a
    kernel extension is loaded, references to functions present in
    nativeos.elf are not initialized because the kernel extension does not
    know the memory address of the function inside nativeos.elf.
    
    The symbol table will keep references to each public function exported
    by nativeos.elf, and the relocation process will replace the dummy
    memory addresses present in the ELF object by their actual values.

---
+++ include/elf/symtab.h
@@ -0,0 +1,52 @@
+/*
+ * This file is part of NativeOS
+ * Copyright (C) 2015-2018 The NativeOS contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file symtab.h
+ * \brief Symbol table
+ *
+ * The symbol table is a required data structure by the kernel in order to
+ * build an index of the different public functions available by the kernel and
+ * other kernel extension modules and their respective memory address.
+ *
+ * The reason behind this is that new modules need to be relocated during load
+ * phase, so it's required to know the exact memory address of each public
+ * function a kernel module may use, in order to set the proper memory address
+ * during this phase.
+ */
+#pragma once
+
+/**
+ * \brief Register a symbol in the symbol tab.
+ * \param name the name of this symbol.
+ * \param addr the memory address of the symbol.
+ */
+void symtab_new_entry (const char * name, uintptr_t addr);
+
+/**
+ * \brief Remove a symbol from the symbol tab.
+ * \param name the name of the symbol.
+ */
+void symtab_del_entry (const char * name);
+
+/**
+ * \brief Get the memory address of a symbol.
+ * \param name the name of the symbol whose memory address to return.
+ * \return the address of the given symbol or NULL if not found.
+ */
+uintptr_t symtab_get_addr (const char * name);
