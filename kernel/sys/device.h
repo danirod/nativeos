728bb48e6ac93d380a1db3faa0a9b53efe236564
Dani Rodríguez <dani@danirod.es>
2021-08-18 01:26:26 +0200

    devmgr: draft a device manager
    A device is a specific piece of hardware exposed through a standard
    interface, allowing the kernel to see hardware as a set of functions,
    regardless of their internal kind.
    
    This commit will add a device manager. The device manager is internally
    a list with functions to add, remove and fetch devices. When installing
    a device, the device is bound to a specific name, which is used later to
    refer to the specific device.
    
    At the moment, a device has a set of functions and callbacks used by the
    kernel to interact with the hardware device:
    
    * read: allows the kernel to read data from the device.
    * write: allows the kernel to send data to the device.
    
    For example, a piece of hardware such as a serial port may be controlled
    using a device. The read and write functions in the device may map to
    internal functions used by the driver, which will exchange a buffer of
    data through the pins of the serial port.
    
    A terminal may be represented as a device as well. Calling the read
    function on the terminal device may cause the recently pressed keys in
    the keybaord to be fetched so that the kernel can see what has been
    typed. Calling the write function on the terminal device could cause
    some characters to be sent to the video memory in order to make them
    visible in the screen.
    
    There is a distinction between a device and a driver. The driver is the
    underlying kernel module that manages pieces of hardware of a specific
    family. Some driver examples:
    
    * COM driver: may manage serial ports.
    * PS2KBD driver: may manage standard PS2 keybaords.
    * CGACON driver: may manage standard CGA text consoles.
    
    When initialized, a driver may spawn zero, one or multiple devices:
    
    * The CGACON driver may spawn a device called FB to represent the
      framebuffer. Writing to this file will send characters to the screen.
      There is only one device in this driver, since there is usually only a
      single framebuffer in the video memory.
    
    * The COM driver may spawn multiple devices called COM1, COM2, COM3...
      to represent the different COM ports on a machine, one per serial port
      (assuming the computer has multiple serial ports available).
      Calling read or write in a specific device may cause data to be sent
      into a specific serial port.

---
+++ kernel/sys/device.h
@@ -0,0 +1,79 @@
+#pragma once
+
+struct chardev;
+struct driver;
+
+typedef struct chardev {
+	struct driver *cd_family;
+	unsigned int (*cd_read)(unsigned char *, unsigned int);
+	unsigned int (*cd_write)(unsigned char *, unsigned int);
+} chardev_t;
+
+typedef struct driver {
+	char *dv_name;
+	unsigned int dv_flags;
+#define DV_FCHARDEV 1
+#define DV_FBLCKDEV 2
+	int (*dv_init)(void);
+	void (*dv_tini)(void);
+} driver_t;
+
+/**
+ * \brief Init the device manager.
+ *
+ * This function must be called prior to attempting to mount any kind of
+ * device in order to initialize the system data structures required to
+ * maintain the device manager itself.
+ */
+void
+device_init(void);
+
+/**
+ * \brief Install a device.
+ *
+ * This function should be called by drivers when attempting to expose a
+ * specific device through the device manager. A name to identify the device
+ * should be given, and the device will be accessible through that name.
+ *
+ * The acceptable return values for this function are negative values for
+ * unsuccessful installations, non-negatives otherwise. The current list of
+ * status codes this function may return are:
+ *
+ * 0: device mounted successfully
+ * -1: kernel error
+ * -2: device name already exists
+ *
+ * \param dev the device to make accessible through the device manager.
+ * \param mtname the name to use when refering to this device.
+ * \return status code with the outcome of the mount operation.
+ */
+int
+device_install(chardev_t *dev, char *mtname);
+
+/**
+ * \brief Uninstall a device.
+ *
+ * This function should be called by drivers when attempting to remove a
+ * previously exposed driver, using its identifier. This function will
+ * silently drop requests to remove devices whose name does not match the
+ * name of any mounted device.
+ *
+ * \param mtname the name of the device to remove from the device manager.
+ */
+void
+device_remove(char *mtname);
+
+/*
+ * UNDOCUMENTED UNSTABLE FUNCTIONS. These functions will eventually be removed
+ * once NativeOS has a VFS and is able to manage the devices through a proper
+ * DEVFS.
+ */
+
+int
+device_open(char *mtname);
+
+unsigned int
+device_read(int fd, unsigned char *buf, unsigned int len);
+
+unsigned int
+device_write(int fd, unsigned char *buf, unsigned int len);
