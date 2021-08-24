24f22f1a99799ef82127390cf662bac93c995cbc
Dani Rodríguez <dani@danirod.es>
2021-08-24 20:19:35 +0200

    main: platform independent main function
    This commit replaces the old i386/kmain into two separate subroutines. A
    subroutine for i386 initialization has been unwrapped into the loader
    code of i386/*/boot.S, to init the i386 hardware components and other
    platform dependent code. A subroutine for the generic kernel
    initialization has been added to kern/kern_main.c, to setup the system
    components that don't depend in the platform itself.

---
+++ kernel/kern/kern_main.c
@@ -0,0 +1,52 @@
+/*
+ * This file is part of NativeOS
+ * Copyright (C) 2015-2021 The NativeOS contributors
+ * SPDX-License-Identifier:  GPL-3.0-only
+ */
+
+#include <sys/device.h>
+#include <sys/stdkern.h>
+
+/**
+ * \file kern/kern_main.c
+ * \brief Kernel main entrypoint
+ *
+ * This function contains code executed right after the platform loader has
+ * finished with the platform-dependent stuff. Code in this module should
+ * finish with the system initialisation and delegate to the init process
+ * to have a proper userland switch. (Or until this is programmed, at least
+ * do something useful).
+ */
+
+// FIXME: These should not be declared here.
+extern void vgatext_install(void);
+
+static void kernel_welcome(void);
+
+/**
+ * \brief Kernel main
+ *
+ * Virtually the main function, although you should know that the platform
+ * loader may have done additional stuff before reaching here. Check out
+ * code preceding a call to kernel_main in the loader code for each platform.
+ */
+void
+kernel_main(void)
+{
+	device_init();
+
+	// TODO: These should be dynamically loaded (linker sets?)
+	vgatext_install();
+
+	kernel_welcome();
+}
+
+#define device_puts(dev, str) \
+	device_write(dev, (unsigned char *) str, strlen(str))
+
+static void
+kernel_welcome(void)
+{
+	int fb = device_open("fb");
+	device_puts(fb, "NativeOS\n");
+}
