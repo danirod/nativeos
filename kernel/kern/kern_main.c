ea4ad6ea43eb278e25496d8da029af387b001188
Dani Rodríguez <dani@danirod.es>
2022-01-23 23:40:04 +0100

    main: cleanup kern_main demo routines
    This commit will remove the file system currently done in kern_main.
    Instead, the contents of the INITRD:/SYSTEM/MOTD.TXT file will be placed
    in the screen if the ramdisk was successfully mounted into the INITRD:
    volume when the system starts.

--- kernel/kern/kern_main.c
+++ kernel/kern/kern_main.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier:  GPL-3.0-only
  */
 
-#include <fs/tarfs/tar.h>
 #include <machine/multiboot.h>
 #include <sys/device.h>
 #include <sys/stdkern.h>
@@ -20,13 +19,6 @@
  * to have a proper userland switch. (Or until this is programmed, at least
  * do something useful).
  */
-
-// FIXME: These should not be declared here.
-extern void null_install(void);
-extern void vgatext_install(void);
-extern void keyboard_install(void);
-extern void pctimer_install(void);
-extern void uart8250_install(void);
 
 static void ramdisk_init(void);
 static void kernel_welcome(void);
@@ -50,12 +42,20 @@ kernel_main(void)
 static void
 ramdisk_init(void)
 {
+	unsigned int i;
 	multiboot_module_t *multiboot_mods;
-	unsigned char *tar_buffer;
+	unsigned char *tar;
 
 	multiboot_mods = (multiboot_module_t *) multiboot_info->mods_addr;
-	tar_buffer = (unsigned char *) multiboot_mods[0].mod_start;
-	vfs_mount("tarfs", "INITRD", tar_buffer);
+	for (i = 0; i < multiboot_info->mods_count; i++) {
+		/* We can afford to strcmp because "ramdisk" is static. */
+		if (!strcmp("ramdisk", (char *) multiboot_mods[0].string)) {
+			/* We found the ramdisk. */
+			tar = (unsigned char *) multiboot_mods[i].mod_start;
+			vfs_mount("tarfs", "INITRD", tar);
+			return;
+		}
+	}
 }
 
 static vfs_node_t *
@@ -76,160 +76,26 @@ fs_write_string(vfs_node_t *node, unsigned int offt, const char *str)
 	return fs_write(node, offt, (unsigned char *) str, strlen(str));
 }
 
-static void dump_path(vfs_node_t *stdout, char *path);
-static void dump_dir(vfs_node_t *stdout, char *path, vfs_node_t *dir);
-
-static void
-dump_dir(vfs_node_t *stdout, char *path, vfs_node_t *dir)
-{
-	unsigned int i = 0;
-	vfs_node_t *node;
-	size_t child_len;
-	char *child_path;
-
-	while ((node = fs_readdir(dir, i++))) {
-		child_len = strlen(path) + strlen(node->vn_name) + 2;
-		child_path = (char *) malloc(child_len);
-		if (child_path) {
-			strcpy(child_path, path);
-			/* If the folder I'm in is not root. */
-			if (node->vn_parent->vn_parent) {
-				strcat(child_path, "/");
-			}
-			strcat(child_path, node->vn_name);
-			dump_path(stdout, child_path);
-			free(child_path);
-		}
-	}
-}
-
-static void
-dump_file(vfs_node_t *stdout, vfs_node_t *node)
-{
-	unsigned int read = 0;
-	char buf[128];
-	unsigned int offset = 0;
-
-	fs_write_string(stdout, 0, "\n");
-	while ((read = fs_read(node, offset, buf, 128)) > 0) {
-		fs_write(stdout, 0, buf, read);
-		offset += read;
-	}
-	fs_write_string(stdout, 0, "\n");
-}
-
-static void
-dump_path(vfs_node_t *stdout, char *path)
-{
-	vfs_node_t *node = fs_resolve(path);
-
-	if (node) {
-		switch (node->vn_flags) {
-		case VN_FREGFILE:
-			fs_write_string(stdout, 0, "[FIL]");
-			break;
-		case VN_FDIR:
-			fs_write_string(stdout, 0, "[DIR]");
-			break;
-		case VN_FCHARDEV:
-			fs_write_string(stdout, 0, "[CHD]");
-			break;
-		case VN_FBLOCKDEV:
-			fs_write_string(stdout, 0, "[BLD]");
-			break;
-		}
-		fs_write_string(stdout, 0, " ");
-		fs_write_string(stdout, 0, path);
-		fs_write_string(stdout, 0, " (");
-		fs_write_string(stdout, 0, node->vn_name);
-		fs_write_string(stdout, 0, ")\n");
-		if (node->vn_flags == VN_FDIR) {
-			dump_dir(stdout, path, node);
-		} else if (node->vn_flags == VN_FREGFILE) {
-			dump_file(stdout, node);
-		}
-	}
-}
-
-static void
-dump_mountpoint(vfs_node_t *stdout, char *name)
-{
-	char *path = malloc(strlen(name) + 3);
-	strcpy(path, name);
-	strcat(path, ":/");
-	dump_path(stdout, path);
-	fs_write_string(stdout, 0, "\n");
-}
-
-static void
-dump_mountpoints(vfs_node_t *stdout)
-{
-	vfs_node_t *rootfs, *mountpoint;
-	unsigned int i = 0;
-
-	rootfs = fs_resolve("ROOT:/");
-	if (rootfs) {
-		while ((mountpoint = fs_readdir(rootfs, i++))) {
-			/* Make sure ROOT is not dumped, infinite loop! */
-			if (strcmp(mountpoint->vn_name, "ROOT")) {
-				dump_mountpoint(stdout, mountpoint->vn_name);
-			}
-		}
-	}
-}
-
-static void
-print_mountpoints(vfs_node_t *stdout)
-{
-	vfs_node_t *rootfs, *mountpoint;
-	unsigned int i = 0;
-
-	rootfs = fs_resolve("ROOT:/");
-	if (rootfs) {
-		fs_write_string(stdout, 0, "Mounted file systems:\n");
-		while ((mountpoint = fs_readdir(rootfs, i++))) {
-			fs_write_string(stdout, 0, mountpoint->vn_name);
-			fs_write_string(stdout, 0, "\n");
-		}
-		fs_write_string(stdout, 0, "\n");
-	} else {
-		fs_write_string(stdout, 0, "Error! No rootfs is present\n\n");
-	}
-}
-
-static unsigned int
-octal2int(char *octstring)
-{
-	unsigned int acc = 0;
-	while (*octstring) {
-		acc = acc * 8 + (*octstring - '0');
-		octstring++;
-	}
-	return acc;
-}
-
 static void
 kernel_welcome(void)
 {
-	vfs_node_t *vtcon, *kbd, *uart;
+	vfs_node_t *vtcon, *motd;
+	int read, offt;
+	char buffer[64];
+
 	vtcon = fs_resolve_and_open("DEV:/vtcon", VO_FWRITE);
-	uart = fs_resolve_and_open("DEV:/uart", VO_FWRITE | VO_FREAD);
 
-	print_mountpoints(vtcon);
-	dump_mountpoints(vtcon);
-
-	fs_write_string(vtcon, 0, "This is the NativeOS VTCON\n");
-	fs_write_string(vtcon, 0, "Press any key to test the keyboard :)\n\n");
-
-	fs_write_string(uart, 0, "This is the NativeOS UART\n");
-	fs_write_string(uart, 0, "Type some characters to get echo:\n\n");
-
-	unsigned char buf[16];
-	unsigned int read;
+	motd = fs_resolve_and_open("INITRD:/SYSTEM/MOTD.TXT", VO_FREAD);
+	if (motd) {
+		offt = 0;
+		while ((read = fs_read(motd, offt, buffer, 64)) > 0) {
+			fs_write(vtcon, 0, buffer, read);
+			offt += read;
+		}
+		fs_write_string(vtcon, 0, "\n");
+		fs_close(motd);
+	}
 	for (;;) {
-		fs_read(vtcon, 0, buf, 16);
-
-		read = fs_read(uart, 0, buf, 16);
-		fs_write(uart, 0, buf, read);
+		fs_read(vtcon, 0, buffer, 64);
 	}
 }
